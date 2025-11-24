b2a21b05f73c28b5f381096bf3891b4d2b850557
Argen21 <argenmv06@gmail.com>
2025-11-23 23:14:46 -0600

    img size update

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -4,18 +4,20 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ProductService {
-    getProductsData() {
-        return [
-          { image: 'v1759950688/carrusel-1_vzkvjy.webp' },
-          { image: 'v1759969327/carrusel-2_oswtjk.webp' },
-          { image: 'v1759950688/carrusel-3_ht1f8i.webp' },
-          { image: 'v1759950699/carrusel-4_bw02x2.webp' },
-          { image: 'v1759950699/carrusel-5_nfrqky.webp' },
-          { image: 'v1759950700/carrusel-6_tlhrdx.webp' },
-          { image: 'v1759969329/carrusel-7_omg8rh.webp' },
-          { image: 'v1759954798/carrusel-8_ekucle.webp' },
-        ];
-    }
+getProductsData() {
+
+  return [
+    { image: "v1759950688/carrusel-1_vzkvjy.webp" },
+    { image: "v1759969327/carrusel-2_oswtjk.webp" },
+    { image: "v1759950688/carrusel-3_ht1f8i.webp" },
+    { image: "v1759950699/carrusel-4_bw02x2.webp" },
+    { image: "v1759950699/carrusel-5_nfrqky.webp" },
+    { image: "v1759950700/carrusel-6_tlhrdx.webp" },
+    { image: "v1759969329/carrusel-7_omg8rh.webp" },
+    { image: "v1759954798/carrusel-8_ekucle.webp" },
+  ];
+}
+
 
     getProductsMini() {
         return Promise.resolve(this.getProductsData().slice(0, 5));
